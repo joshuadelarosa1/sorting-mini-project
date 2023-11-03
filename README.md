17a9b9a5679efc396f6822c5c407816f31fa4455
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-11-02 22:23:43 -0500

    merge sort implemented

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Authors: Joshua Delarosa, Sam Rebelsky, MIT
 This project implements different ways to sort including: insertion sort,
 merge sort, quick sort, and a new type of sort.
 
-Resources used: Notes from class
+Resources used: Notes from class, Worked with Alma Ordaz on merge sort, Lab
+with Luke Walters was used for the Quicksort
 https://www.baeldung.com/junit5-assertall-vs-multiple-assertions
 https://www.geeksforgeeks.org/java-integer-compare-method/#
 
--- src/MergeSort.java
+++ src/MergeSort.java
@@ -24,15 +24,58 @@ public class MergeSort implements Sorter {
   /**
    * Create a sorter.
    */
-  MergeSort() {
-  } // MergeSort()
+  MergeSort() {} // MergeSort()
 
   // +---------+-----------------------------------------------------
   // | Methods |
   // +---------+
 
+  /**
+   * Merge the values from positions [lo..mid) and [mid..hi) back into the same part of the array.
+   *
+   * Preconditions: Each subarray is sorted accorting to comparator.
+   */
+  static <T> void merge(T[] vals, int lo, int mid, int hi, Comparator<? super T> comparator) {
+    T[] temp = java.util.Arrays.copyOf(vals, vals.length);
+    int i = lo;
+    int originalMid = mid;
+
+    while (lo != originalMid && mid != hi) {
+      if ((comparator.compare(vals[lo], vals[mid])) <= 0) {
+        temp[i++] = vals[lo++];
+      } else if ((comparator.compare(vals[lo], vals[mid]) > 0)) {
+        temp[i++] = vals[mid++];
+      }
+    }
+
+    if (lo < originalMid) {
+      while (i != hi) {
+        temp[i++] = vals[lo++];
+      }
+    }
+
+    for (int j = 0; j < vals.length; j++) {
+      vals[j] = temp[j];
+    }
+  } // merge
+
+  public <T> void mergeSort(T[] values, int lb, int ub, Comparator<? super T> comparator) {
+    int mid = lb + ((ub - lb) / 2);
+
+    if (ub - lb > 1) {
+      mergeSort(values, lb, mid, comparator);
+      mergeSort(values, mid, ub, comparator);
+      merge(values, lb, mid, ub, comparator);
+    }
+  } // mergeSort
+
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    // STUB
+    if (values.length <= 1) {
+      return;
+    }
+
+    mergeSort(values, 0, values.length, order);
   } // sort(T[], Comparator<? super T>
+
 } // class MergeSort
