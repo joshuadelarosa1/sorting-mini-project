2e00fa89e321ae8b471c0e50d3fa3efb3c5c987e
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-11-02 23:53:17 -0500

    quicksort implemented

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -31,6 +31,10 @@ public class Quicksort implements Sorter {
   // +---------+
 
   static <T> int partition(T[] arr, int lb, int ub, Comparator<? super T> order) {
+    if (arr.length <= 1) {
+      return 0;
+    }
+
     int small = lb + 1;
     int large = ub;
     int pivotIndex = lb + ((ub - lb) / 2);
@@ -42,14 +46,13 @@ public class Quicksort implements Sorter {
     while (small < large) {
       if (order.compare(arr[small], arr[lb]) <= 0) {
         small++;
-      } else if (order.compare(arr[large], arr[lb]) > 0) {
+      } else if (order.compare(arr[large - 1], arr[lb]) > 0) {
         large--;
       } else {
         temp = arr[small];
-        arr[small] = arr[large];
+        arr[small] = arr[--large];
         arr[large] = temp;
         small++;
-        large--;
       }
     }
 
@@ -62,10 +65,6 @@ public class Quicksort implements Sorter {
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    if (values.length <= 1) {
-      return;
-    }
-
     partition(values, 0, values.length, order);
   } // sort(T[], Comparator<? super T>
 } // class Quicksort
