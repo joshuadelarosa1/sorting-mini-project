01b64e87fe0a75de2c85fdc4e3361effe10e5dac
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-11-02 22:46:16 -0500

    readme update

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ with Luke Walters was used for the Quicksort
 https://www.baeldung.com/junit5-assertall-vs-multiple-assertions
 https://www.geeksforgeeks.org/java-integer-compare-method/#
 
+I used chat GPT for my own sort. I made very little changes, as I did not
+know what else to do. My experience with chat GPT was very lovely, it gave me
+some sanity. I had to give it a few different prompts in order to get what I 
+actually wanted. The code otherwise was pretty easy to read, a little bit
+hard to understand what was going on, but I think I get it conceptually. 
--- src/Quicksort.java
+++ src/Quicksort.java
@@ -30,41 +30,6 @@ public class Quicksort implements Sorter {
   // | Methods |
   // +---------+
 
-  static <T> void partition(T[] arr, int lb, int ub, int pivotIndex) {
-    T temp;
-    temp = arr[ub];
-    arr[ub] = arr[pivotIndex];
-    arr[pivotIndex] = temp;
-    int small = lb;
-    int large = ub - 1;
-    while (small != large) {
-      if ((arr[small] >= arr[ub]) && (arr[large] <= arr[ub])) {
-        temp = arr[small];
-        arr[small] = arr[large];
-        arr[large] = temp;
-        small++;
-        large--;
-      } else if ((arr[small] >= arr[ub]) && (arr[large] >= arr[ub])) {
-        large--;
-      }
-
-      else if ((arr[small] <= arr[ub]) && (arr[large] <= arr[ub])) {
-        small++;
-      } else if ((arr[small] <= arr[ub]) && (arr[large] >= arr[ub])) {
-        small++;
-        large--;
-      }
-
-    }
-    temp = arr[large];
-    arr[large] = arr[ub];
-    arr[ub] = temp;
-    for (int i : arr) {
-      System.out.print(i + " ");
-    }
-    System.out.println();
-    return large;
-  }
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
