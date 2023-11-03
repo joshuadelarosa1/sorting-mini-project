ee093a5b5dc5defbe3ead958477011e125e48ebe
joshuadelarosa1 <118570698+joshuadelarosa1@users.noreply.github.com>
2023-11-02 23:19:07 -0500

    quicksort implemented

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -30,9 +30,52 @@ public class Quicksort implements Sorter {
   // | Methods |
   // +---------+
 
+  static <T> void partition(T[] arr, int lb, int ub, Comparator<? super T> order) {
+    T temp;
+    int pivotIndex = lb + ((ub - lb) / 2);
+    int small = lb;
+    int large = ub - 1;
+
+    temp = arr[large];
+    arr[large] = arr[pivotIndex];
+    arr[pivotIndex] = temp;
+
+    while (small != large) {
+      if(large < 0) {
+        break;
+      }
+
+      if (order.compare(arr[small], arr[ub]) >= 0 && order.compare(arr[large], arr[ub]) <= 0) {
+        temp = arr[small];
+        arr[small] = arr[large];
+        arr[large] = temp;
+
+        small++;
+        large--;
+      } else if (order.compare(arr[small], arr[ub]) >= 0
+          && order.compare(arr[large], arr[ub]) >= 0) {
+        large--;
+      } else if (order.compare(arr[small], arr[ub]) <= 0
+          && order.compare(arr[large], arr[ub]) <= 0) {
+        small++;
+      } else if (order.compare(arr[small], arr[ub]) <= 0
+          && order.compare(arr[large], arr[ub]) >= 0) {
+        small++;
+        large--;
+      }
+
+    }
+    temp = arr[large];
+    arr[large] = arr[ub];
+    arr[ub] = temp;
+  } // partition
 
   @Override
   public <T> void sort(T[] values, Comparator<? super T> order) {
-    
+    if (values.length <= 1) {
+      return;
+    }
+
+    partition(values, 0, values.length - 1, order);
   } // sort(T[], Comparator<? super T>
 } // class Quicksort
